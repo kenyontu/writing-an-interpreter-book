8a24657f1389372618e31e8c0a4867ed8178617f
Kenyon Tu <kenyontu@outlook.com>
2025-01-22 16:58:58 +0800

    basic prompt

---
+++ src/repl.rs
@@ -0,0 +1,31 @@
+use std::io::{self, Write};
+
+use crate::{lexer::Lexer, token::TokenType};
+
+pub fn start() {
+    let mut input = String::new();
+    loop {
+        print!(">> ");
+        io::stdout().flush().unwrap();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                if input == "\n" {
+                    break;
+                }
+
+                println!("input: {input}");
+
+                let mut lexer = Lexer::new(&input);
+                let mut token = lexer.next_token();
+
+                while token.token_type != TokenType::Eof {
+                    println!("{:?}", token);
+                    token = lexer.next_token();
+                }
+
+                print!("\n");
+            }
+            Err(error) => println!("Error: {error}"),
+        }
+    }
+}
